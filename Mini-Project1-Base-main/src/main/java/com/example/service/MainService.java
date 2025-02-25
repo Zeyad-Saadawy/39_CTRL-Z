80f22df264713e51c03b306028aa8d489758c60e
MouradGalal <mouradgalal30@gmail.com>
2025-02-25 20:53:37 +0200

    MainService implementation

--- Mini-Project1-Base-main/src/main/java/com/example/service/MainService.java
+++ Mini-Project1-Base-main/src/main/java/com/example/service/MainService.java
@@ -1,16 +1,40 @@
 package com.example.service;
 
-
+import com.example.repository.MainRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Primary
 @Service
 public abstract class MainService<T> {
 
+    private final MainRepository<T> repository;
 
-    
+    @Autowired
+    public MainService(MainRepository<T> repository) {
+        this.repository = repository;
+    }
 
-    
-    
+    // Common method to get all entities
+    public ArrayList<T> findAll() {
+        return repository.findAll();
+    }
+
+    // Common method to save an entity
+    public void save(T entity) {
+        repository.save(entity);
+    }
+
+    // Common method to save all entities
+    public void saveAll(ArrayList<T> entities) {
+        repository.saveAll(entities);
+    }
+
+    // Common method to override data
+    public void overrideData(ArrayList<T> data) {
+        repository.overrideData(data);
+    }
 }

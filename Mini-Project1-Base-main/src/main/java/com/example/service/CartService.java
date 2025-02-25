ce6e6a0f7d869101ef6c280d8e1abeed9f4c13ea
Zeyad Hesham <zeyadhesham53@gmail.com>
2025-02-25 22:16:49 +0200

    calculateTotalPrice for CartService

--- Mini-Project1-Base-main/src/main/java/com/example/service/CartService.java
+++ Mini-Project1-Base-main/src/main/java/com/example/service/CartService.java
@@ -1,4 +1,14 @@
 package com.example.service;
 
 public class CartService {
+    // CartService.java
+    public double calculateTotalPrice(Cart cart) {
+        if(cart == null || cart.getProducts().isEmpty()) {
+            return 0.0;
+        }
+
+        return cart.getProducts().stream()
+                .mapToDouble(Product::getPrice)
+                .sum();
+    }
 }

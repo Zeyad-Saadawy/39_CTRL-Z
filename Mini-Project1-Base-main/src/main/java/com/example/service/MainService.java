576ddd023260e854e68b744a47963de4530e3f66
Zeyad Hesham <zeyadhesham53@gmail.com>
2025-02-26 17:48:55 +0200

    Main repo should not have @autowired

--- Mini-Project1-Base-main/src/main/java/com/example/service/MainService.java
+++ Mini-Project1-Base-main/src/main/java/com/example/service/MainService.java
@@ -2,38 +2,30 @@ package com.example.service;
 
 import com.example.repository.MainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 
-@Primary
 @Service
 public abstract class MainService<T> {
 
-    private final MainRepository<T> repository;
+    protected final MainRepository<T> repository; // Use protected for subclass access
 
-    @Autowired
     public MainService(MainRepository<T> repository) {
         this.repository = repository;
     }
 
-    // Common method to get all entities
     public ArrayList<T> findAll() {
         return repository.findAll();
     }
 
-    // Common method to save an entity
     public void save(T entity) {
         repository.save(entity);
     }
 
-    // Common method to save all entities
     public void saveAll(ArrayList<T> entities) {
         repository.saveAll(entities);
     }
 
-    // Common method to override data
     public void overrideData(ArrayList<T> data) {
         repository.overrideData(data);
     }
